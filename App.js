5c02573e1c3794360ba811cfe8cff4502bc0c272
ParkJongJoon <whdwnsdk8111@naver.com>
2022-05-01 15:18:23 +0900

    Divide screen

--- App.js
+++ App.js
@@ -1,29 +1,15 @@
 import { React } from 'react';
-import { StyleSheet, SafeAreaView, View } from 'react-native';
-import Buttons from './components/Buttons';
+import { StyleSheet} from 'react-native';
+import MainScreen from './screen/MainScreen'
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.full}>
-      <View style={styles.view1} />
-      <View style={styles.view2}>
-        <Buttons />
-      </View>
-
-    </SafeAreaView>
+    <MainScreen />
   );
 }
 
 const styles = StyleSheet.create({
-  full: {
-    flex: 1,
-  },
-  view1:{
-    flex: 3,
-  },
-  view2:{
-    flex:2,
-  },
+  
 });
 
 export default App;
---
+++ screen/MainScreen.js
@@ -0,0 +1,28 @@
+import { React } from 'react';
+import { StyleSheet, SafeAreaView, View } from 'react-native';
+import Buttons from '../components/Buttons';
+
+function MainScreen() {
+    return (
+        <SafeAreaView style={styles.full}>
+            <View style={styles.view1} />
+            <View style={styles.view2}>
+                <Buttons />
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    full: {
+        flex: 1,
+    },
+    view1: {
+        flex: 3,
+    },
+    view2: {
+        flex: 2,
+    },
+});
+
+export default MainScreen;
